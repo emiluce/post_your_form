ce3e1ae7b75257edb9c553bc4c6d3b2b702441d4
emi.luciani18@gmail.com <59806361+emiluce@users.noreply.github.com>
2020-03-11 17:23:53 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "./style.css";
+import axios from "axios";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      poster: "",
+      comment: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    this.fetchAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Your favorite movie</h1>
+        <form onSubmit={this.submitForm}>
+          <input
+            type="text"
+            id="name-movie"
+            name="title"
+            placeholder="Movie"
+            onChange={this.onChange}
+            value={this.state.name}
+          />
+          <br />
+          <input
+            type="text"
+            id="poster"
+            name="poster"
+            placeholder="Post the link here"
+            onChange={this.onChange}
+            value={this.state.poster}
+          />
+          <br />
+          <textarea
+            name="comment"
+            id="comment"
+            cols="30"
+            rows="10"
+            placeholder="Why do you like this movie?"
+            onChange={this.onChange}
+            value={this.state.comment}
+          ></textarea>
+          <br />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    );
+  }
+
+  fetchAPI = async () => {
+    const url = "https://post-a-form.herokuapp.com/api/movies/";
+
+    const response = await axios.post(url, this.state);
+
+    alert("The movie has been added");
+  };
+}
+
+export default App;
